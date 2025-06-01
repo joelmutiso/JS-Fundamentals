715192d0512c75a36293d1e42643a643917096d3
Joel Mutiso <joelwmutiso@gmail.com>
2025-06-02 00:26:16 +0300

    Fix: Correct output for 7-multi_c.js task

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,14 @@
+// Convert first argument to integer
 const x = parseInt(process.argv[2]);
 
-if (!x || x <= 0) {
+// Check if x is missing or not a number
+if (!process.argv[2] || isNaN(x)) {
+  // Print error message if no valid number is provided
   console.log("Missing number of occurrences");
-} else {
-  let output = "";
+} else if (x > 0) {
+  // If x is positive, print "C is fun" x times
   for (let i = 0; i < x; i++) {
-    output += "C is fun\n";
+    console.log("C is fun");
   }
-  console.log(output.trim());
 }
+// If x is zero or negative, do nothing (print nothing)
