2f6fbf1aecb3d4579c8eddcd866bdba22345be1b
Joel Mutiso <joelwmutiso@gmail.com>
2025-06-01 23:36:00 +0300

    print multi-language messages script

---
+++ 1-multi_languages.js
@@ -0,0 +1,7 @@
+const myMessage1 = "C is fun";
+const myMessage2 = "Python is cool";
+const myMessage3 = "JavaScript is amazing";
+
+console.log(myMessage1);
+console.log(myMessage2);
+console.log(myMessage3);
